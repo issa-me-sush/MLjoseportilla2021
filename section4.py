ce485aa230d7406d2512ef18805a4ee9e7184ec6
sush <coolsushaan@gmail.com>
2021-08-18 21:17:44 +0530

    test

--- section4.py
+++ section4.py
@@ -1,1 +1,15 @@
-import
+import numpy as np
+
+mylist = [1,4,560]
+myarr = np.array(mylist)
+print(type(mylist))
+print(type(myarr))
+print(mylist)
+print(myarr)
+list2 = [[1,2,3],[4,5,6],[7,8,9]]
+arr2 = np.array(list2)
+print(list2)
+print(arr2)
+
+arr3 = np.arange(0,5)
+print(arr3)
